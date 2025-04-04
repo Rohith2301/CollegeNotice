4e998cff89293db04f59cc8eb2100cab673c515e
Rohith <rollarohith23@gmail.com>
2025-04-04 19:38:56 +0530

    updated Tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomeURLTest(TestCase):
+    def test_login_url_exists(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+class SignupURLTest(TestCase):
+    def test_SignUp_url_exists(self):
+        response = self.client.get(reverse('student_login'))
+        self.assertEqual(response.status_code, 200)
+
+class AboutURLTest(TestCase):
+    def test_about_url_exists(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
